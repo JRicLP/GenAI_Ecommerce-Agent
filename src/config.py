bab74ba667e67a4890ce82d1ae58815ef34efa6b
Ricardo <joaoricardolopes2013@gmail.com>
2026-04-18 14:27:30 -0300

    Fix raise ValueError message.

--- src/config.py
+++ src/config.py
@@ -8,4 +8,5 @@ load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 if not GEMINI_API_KEY:
-    raise ValueError("A chave de API do Gemini não foi encontrada. Por favor, defina a variável de ambiente GEMINI_API_KEY.")
+    raise ValueError("A chave de API do Gemini não foi encontrada." \
+    "Por favor, defina a variável de ambiente GEMINI_API_KEY.")
